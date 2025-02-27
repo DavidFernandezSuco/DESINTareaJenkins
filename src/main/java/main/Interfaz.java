069ef8d84a5a84fb75e1223c1f0ef9df254c101d
DavidFernandezSuco <150021105+DavidFernandezSuco@users.noreply.github.com>
2025-02-27 12:09:35 +0100

    Update Interfaz.java

--- src/main/java/main/Interfaz.java
+++ src/main/java/main/Interfaz.java
@@ -3,10 +3,15 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Interfaz {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +43,21 @@ public class Interfaz {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Introduce tu nombre:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(56, 34, 222, 45);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(52, 91, 199, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnNewButton.setBounds(152, 168, 143, 45);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
